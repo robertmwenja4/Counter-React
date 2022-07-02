521f7bd140f4a75da324b9896b0297b25ef22010
robertmwenja4 <robertmwenja4@gmail.com>
2022-07-02 23:13:38 +0300

    Multiple counters Added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,29 +2,40 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: []
+        value: this.props.counter.value
+        /* tags: [] */
     };
+
+    //Use it to bind event handler but you can also use Arrow function
+    /* constructor(){
+        super();
+        this.handleIncrement = this.handleIncrement.bind(this);
+    } */
+    
     //Custom Styling using js object then call using this keyword
-    style = {
-        fontSize: 10,//jsx converts this to "10px"
-        fontWeight: 'bold'
-    }
+    // style = {
+    //     fontSize: 10,//jsx converts this to "10px"
+    //     fontWeight: 'bold'
+    // }
     /* returnEvaluate(){
         if (this.state.tags.length === 0) return <p>There is no Tags</p>;
         return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
     } */
-    handleIncrement(){
-        console.log("Increment by 1");
+    handleIncrement = ()=>{
+        // console.log(product);
+        this.setState({ value: this.state.value + 1 });
     }
     render() { 
-        
+        console.log(this.props)
         return (
-            <div className="mt-5">
+            <div className="mt-2">
             {/* style={this.style}
             style={{ fontSize: 30 }} */}
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            
             <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            {/* Here we raise an event which will be handled in the Counters Component */}
+            <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             {/* //Using truthy and falsy */}
             {/* {this.state.tags.length === 0 && "Create new tags!"} 
             {this.returnEvaluate()} */}
@@ -33,12 +44,12 @@ class Counter extends Component {
     }
     getBadgeClasses(){
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
     formatCount(){
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
+        const { value } = this.state;
+        return value === 0 ? "Zero" : value;
     }
 }
  
--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import Counters from './components/counters';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Counter />
-  </React.StrictMode>
-);
+root.render( < Counters / > );
 
-// If you want to start measuring performance in your app, pass a function
+/* // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals(); */
